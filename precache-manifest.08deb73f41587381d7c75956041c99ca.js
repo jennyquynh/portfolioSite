50c29d9aed09f51eca3848c3b4eb33607ddcae58
Jenny Nguyen <jennyquynhphuong@gmail.com>
2019-10-24 20:57:03 -0500

    Updates

---
+++ precache-manifest.08deb73f41587381d7c75956041c99ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5a629ff13fe1fc601e36908bd87b7f",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "29e49e54306383dee20f",
+    "url": "/client/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "1c0ce705ef02dc79d2bf",
+    "url": "/client/static/css/main.3923468f.chunk.css"
+  },
+  {
+    "revision": "29e49e54306383dee20f",
+    "url": "/client/static/js/2.4c770371.chunk.js"
+  },
+  {
+    "revision": "1c0ce705ef02dc79d2bf",
+    "url": "/client/static/js/main.747ca771.chunk.js"
+  },
+  {
+    "revision": "8c9abfac92a1c514316a",
+    "url": "/client/static/js/runtime-main.bac1d710.js"
+  }
+]);
