e5f7e288b338b5865f82f1b2c31ae2038eced1ac
Jenny Nguyen <jennyquynhphuong@gmail.com>
2019-10-24 21:35:41 -0500

    Updates

--- precache-manifest.20e439440376259af576562767dfc426.js
+++ precache-manifest.20e439440376259af576562767dfc426.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a98be6b62c2e318b400bbb2d0d0f480",
+    "revision": "2502247fd948082564dd25f9ce25f9f3",
     "url": "/portfolioSite/index.html"
   },
   {
-    "revision": "e74e767ea292ec0deaf9",
+    "revision": "3b5026b048af4b4c504a",
     "url": "/portfolioSite/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "55ac65f4979dd7fec2b5",
+    "revision": "5aaf8165d97c19136d95",
     "url": "/portfolioSite/static/css/main.3923468f.chunk.css"
   },
   {
-    "revision": "e74e767ea292ec0deaf9",
-    "url": "/portfolioSite/static/js/2.29a6dc27.chunk.js"
+    "revision": "3b5026b048af4b4c504a",
+    "url": "/portfolioSite/static/js/2.772fdcb2.chunk.js"
   },
   {
-    "revision": "55ac65f4979dd7fec2b5",
-    "url": "/portfolioSite/static/js/main.e33b7096.chunk.js"
+    "revision": "5aaf8165d97c19136d95",
+    "url": "/portfolioSite/static/js/main.704a5e46.chunk.js"
   },
   {
     "revision": "bd4993c8b4c819c5f59a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolioSite/precache-manifest.014f9604bed4c6974786393ec07272a4.js"
+  "/portfolioSite/precache-manifest.20e439440376259af576562767dfc426.js"
 );
 
 self.addEventListener('message', (event) => {
